8f3b956828b38cd743215988f470f0a1aefd2917
Dave Yoon <daveyoon64@gmail.com>
2018-06-07 02:02:01 +0000

    Container is running, but gremlin.yaml still needs to be configured

--- setup.sh
+++ setup.sh
@@ -1,12 +1,24 @@
-!/usr/bin/env bash
-# This script will set up a server to run the socks-k8 deployment
+#!/usr/bin/env bash
+# Setup master node of K8S cluster
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl docker.io
 
-# Set up kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+# initialize the master node
+kubeadm init
+# need to test this to make sure I'm saving the last line
+# $K8JOIN=$(kubeadm init | tail -1)
 
-# Set up minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.27.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+# Create a new user (k8s requires a non-root user to use)
+useradd socksadmin
+usermod -aG sudo socksadmin
 
-
+# switch to the new user
+su socksadmin
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
